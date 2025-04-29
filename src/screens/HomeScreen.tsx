f96a46b5648e953afd887dea77189b165e2d2a28
lgm1007 <flxl654@gmail.com>
2025-04-29 16:02:43 +0900

    feat: HomeScreen 스크린 추가 및 App에서 HomeScreen 띄우기

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>GymNote 홈 화면입니다</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
